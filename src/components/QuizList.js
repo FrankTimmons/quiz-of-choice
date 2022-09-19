5dcf9b980997fc5d5fb19081ff4526f88cc11885
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-09-19 18:17:01 -0400

    update quiz list to display name of quiz

---
+++ src/components/QuizList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Quiz from "./Quiz";
+import PropTypes from "prop-types";
+
+function QuizList(props){
+
+  return(
+    <React.Fragment>
+      <hr/>
+      {props.quizList.map((quiz) =>
+        <Quiz 
+          whenQuizClicked={props.onQuizSelection}
+          name={quiz.name}
+          id={quiz.id}
+          key={quiz.id}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+QuizList.propTypes = {
+  quizList: PropTypes.array,
+  onQuizSelection: PropTypes.func,
+};
+
+export default QuizList;
+
