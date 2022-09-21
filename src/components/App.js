c984187e24712064e05b7d73b261e796cc6d1dc3
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-09-21 12:57:45 -0400

    update Header to use styled components

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,23 @@ import Header from './Header';
 import QuizControl from './QuizControl';
 import SignIn from './SignIn';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.section`
+  background-color: red;
+`;
 
 function App() {
   return (
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path='/sign-in' element={<SignIn/>}/>
-        <Route path='/' element={<QuizControl/>}/>       
-      </Routes>
-    </Router>
+    <StyledWrapper>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path='/sign-in' element={<SignIn/>}/>
+          <Route path='/' element={<QuizControl/>}/>       
+        </Routes>
+      </Router>
+    </StyledWrapper>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const QuizOfChoiceHeader = styled.h1`
+font-size: 24px;
+text-align: center;
+color: white;
+// background-color: teal;
+`;
+
+const StyledWrapper = styled.section`
+  background-color: gray;
+`;
 
 function Header() {
   return (
     <React.Fragment>
-      <h1>Quiz of Choice!</h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/sign-in">Account</Link>
-        </li>
-      </ul>
+      <StyledWrapper>
+      <QuizOfChoiceHeader>
+        Quiz of Choice
+      </QuizOfChoiceHeader>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/sign-in">Account</Link>
+          </li>
+        </ul>
+      </StyledWrapper>
     </React.Fragment>
   );
 }
